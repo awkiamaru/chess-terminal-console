13ed8adf5e4652a59c8f7f05e5218cdac0c3129f
LuisFrag <luispoma.frag@gmail.com>
2020-01-02 12:23:22 -0300

    Update Maven itens

--- src/br/com/chess/boardgame/exceptions/BoardException.java
+++
@@ -1,9 +0,0 @@
-package br.com.chess.boardgame.exceptions;
-
-public class BoardException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public BoardException(String msg){
-        super(msg);
-    }
-}
--- src/br/com/chess/chess/utils/Color.java
+++
@@ -1,6 +0,0 @@
-package br.com.chess.chess.utils;
-
-public enum  Color {
-    BLACK,
-    WHITE;
-}
