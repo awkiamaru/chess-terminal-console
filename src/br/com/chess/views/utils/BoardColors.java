62559315c6e7cba945492044f9b6fa847c673e53
LuisFrag <luispoma.frag@gmail.com>
2020-01-02 01:36:17 -0300

    Update Initialize game

--- src/br/com/chess/views/utils/BoardColors.java
+++ src/br/com/chess/views/utils/BoardColors.java
@@ -11,12 +11,12 @@ public class BoardColors {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    protected static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    protected static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    protected static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    protected static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    protected static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    protected static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    protected static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 }
