d08754f8b6d7654d2c9dd0bfd08ae9b188ae3690
LuisFrag <luispoma.frag@gmail.com>
2020-01-01 21:30:38 -0300

    create piece bishop implement logic

--- src/br/com/chess/chess/ChessMatch.java
+++ src/br/com/chess/chess/ChessMatch.java
@@ -5,6 +5,7 @@ import br.com.chess.boardgame.Piece;
 import br.com.chess.boardgame.Position;
 import br.com.chess.chess.exceptions.ChessException;
 import br.com.chess.chess.utils.Color;
+import br.com.chess.pieces.Bishop;
 import br.com.chess.pieces.King;
 import br.com.chess.pieces.Pawn;
 import br.com.chess.pieces.Rook;
@@ -197,7 +198,9 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -209,7 +212,9 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
